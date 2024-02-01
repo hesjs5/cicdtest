754ce5a1f64c32a0edc4c61e2f791a4732586b9d
harrie88 <84639009+hesjs5@users.noreply.github.com>
2024-02-01 09:06:05 +0900

    Delete mybatis_practice directory

--- mybatis_practice/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS MEMBER;
-
-CREATE TABLE MEMBER
-(
-    id bigint auto_increment primary key,
-    name varchar(20) not null,
-    age int not null,
-    grade varchar(20) not null
-);
--- mybatis_practice/src/test/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO MEMBER (name, age) VALUES ('test1', 11);
-INSERT INTO MEMBER (name, age) VALUES ('test2', 12);
-INSERT INTO MEMBER (name, age) VALUES ('test3', 13);
-INSERT INTO MEMBER (name, age) VALUES ('test4', 14);
